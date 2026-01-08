7d94c8591ceb2da9d4bc2883bd9d144122cdcf15
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-01-08 12:03:07 +0100

    Line class(lign.php)

---
+++ app/models/Ligne.php
@@ -0,0 +1,44 @@
+<?php
+class Line {
+    private $conn;
+    private $table = 'lines';
+    
+    public $id;
+    public $name;
+    public $code;
+    
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+    
+    public function getAll() {
+        $query = "SELECT * FROM " . $this->table . " ORDER BY name";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public function getById($id) {
+        $query = "SELECT * FROM " . $this->table . " WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    
+    public function getLinesByStation($station_id) {
+        $query = "SELECT l.* 
+                  FROM " . $this->table . " l
+                  JOIN line_stations ls ON l.id = ls.line_id
+                  WHERE ls.station_id = :station_id";
+        
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':station_id', $station_id);
+        $stmt->execute();
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
